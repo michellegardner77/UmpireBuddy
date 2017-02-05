e33052e722200a57075adb35a9f589be7318bc82
Michelle Gardner <mgm7c@mail.umkc.edu>
2017-02-05 00:22:18 -0600

    I updated files, created out count UI, and methods to update it. I created a launch icon.

--- app/src/main/java/com/mgard/umpirebuddy/MainActivity.java
+++ app/src/main/java/com/mgard/umpirebuddy/MainActivity.java
@@ -7,16 +7,30 @@ package com.mgard.umpirebuddy;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private int strike_count = 0;
 
     private int ball_count = 0;
+
+    private int out_count = 0;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     // Method that creates the activity; It is also used to enable the OnClickListener on the strike and ball buttons
     @Override
@@ -41,12 +55,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         BallButton.setOnClickListener(this);
 
         updateBallCount();
+
+        // TODO: check if there is a storage value before setting to 0. need method to set out_count
+        updateOutCount();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     // Method used to set the StrikeCount to the strike_count global variable
     private void updateStrikeCount() {
         // Reference the strike_count_value TextView on the activity layout created a new TextView object.
-        TextView strikeCountValue = (TextView)findViewById(R.id.strike_count_value);
+        TextView strikeCountValue = (TextView) findViewById(R.id.strike_count_value);
 
         // Set the text of the strikeCountValue TextView to the values of the strike_count global variable.
         strikeCountValue.setText(Integer.toString(strike_count));
@@ -54,18 +75,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // Method used to set the BallCount to the ball_count global variable
     private void updateBallCount() {
-        TextView ballCountValue = (TextView)findViewById(R.id.ball_count_value);
+        TextView ballCountValue = (TextView) findViewById(R.id.ball_count_value);
         ballCountValue.setText(Integer.toString(ball_count));
     }
 
+    private void updateOutCount() {
+        TextView outCountValue = (TextView) findViewById(R.id.ball_count_value);
+        outCountValue.setText(Integer.toString(out_count));
+
+        // TODO: save to storage
+    }
+
     // method to reset the counts of strike and ball
-    private void resetCounts(){
+    private void resetStrikeBallCounts() {
         ball_count = 0;
         strike_count = 0;
 
         updateStrikeCount();
         updateBallCount();
     }
+
+    private void resetOutCount() {
+        out_count = 0;
+        updateOutCount();
+
+        // TODO: clear storage value
+    }
+
 
 
     // Will send in variable v and choose case that is appropriate, updating either the ball or strike count
@@ -81,7 +117,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     builder.setCancelable(false);
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-                            resetCounts();
+                            resetStrikeBallCounts();
+                            out_count++;
+                            updateOutCount();
                         }
                     });
                     builder.show();
@@ -98,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     builder.setCancelable(false);
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
-                            resetCounts();
+                            resetStrikeBallCounts();
                         }
                     });
                     builder.show();
@@ -108,5 +146,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
